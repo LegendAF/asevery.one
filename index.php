01a44e18007c3926aa591926eee6bf7a40dcd3cc
Alex Fernandez <afernandez@rv-clt-afernandez.redventures.net>
2015-11-13 23:02:58 -0500

    yeah

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
-<img src="assets/gifs/1.gif">
-<img src="assets/gifs/2.jpg">
-<img src="assets/gifs/3.jpg">
-<img src="assets/gifs/4.jpg">
-<img src="assets/gifs/5.jpg">
-<img src="assets/gifs/6.jpg">
-<img src="assets/gifs/7.jpg">
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>title</title>
+        <link rel="stylesheet" href="/assets/css/project.min.css">
+    </head>
+    <body>
+        <div class="gif"><img src="assets/gifs/1.gif"></div>
+        <div class="gif"><img src="assets/gifs/2.gif"></div>
+        <div class="gif"><img src="assets/gifs/3.gif"></div>
+        <div class="gif"><img src="assets/gifs/4.gif"></div>
+        <div class="gif"><img src="assets/gifs/5.gif"></div>
+        <div class="gif"><img src="assets/gifs/6.gif"></div>
+        <div class="gif"><img src="assets/gifs/7.gif"></div>
+        <div class="gif"><img src="assets/gifs/8.gif"></div>
+        <div class="gif"><img src="assets/gifs/9.gif"></div>
+        <div class="gif"><img src="assets/gifs/10.gif"></div>
+        <div class="gif"><img src="assets/gifs/11.gif"></div>
+    </body>
+</html>
